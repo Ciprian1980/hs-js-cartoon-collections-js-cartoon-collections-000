c019e456b07ed59ebcfa36429d5edb1f81372590
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-12-06 03:50:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function dwarfRollCall(dwarves) {
+  var lineDwarves;
+  for ( let d = 0;  d < dwarves.length; d++){
+  
+  lineDwarves += (d+1) .toString() + ". "  +  dwarves[d] + " ";
+  }
+  return lineDwarves;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var linePlaneteers = [ ];
+  
+  for ( let p = 0; p < planeteerCalls.length; p++){
+    linePlaneteers.push(planeteerCalls.toUpperCase(p) + "!");
+  }
+  return linePlaneteers;
 }
 
+
 function longPlaneteerCalls(words) {
+  var arrayCalls = [];
+ // if ( words > 4 )
+  
 }
 
 function findTheCheese (foods) {

7a25a73df1116c927921ff5a8e85f30c684f9878
Ciprian Popa <ciprian_popa2002@yahoo.com>
2019-06-16 13:56:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function dwarfRollCall(arrayDwarves){
   let stringDwarves = "";
   for( let i = 0; i < arrayDwarves.length; i++){
-  arrayDwarves += ( i + 1 + ". " + arrayDwarves[i] + " " );
+  stringDwarves += ( i + 1 + ". " + arrayDwarves[i] + " " );
   }
   return stringDwarves;
 }
 //"1. Doc 2. Dopey 3. Bashful 4. Grumpy "
+
+function summonCaptainPLanet(planeteerCalls){
+  let newArray = [];
+  for(let i = 0; i < planeteerCalls.length; i++){
+    newArray.push(planeteerCalls[i].toUpperCase + "! " );
+  }
+  return newArray;
+}
 
 

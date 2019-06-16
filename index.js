88eb48f7918f8492c57ac309106023b9f35dec7b
Ciprian Popa <ciprian_popa2002@yahoo.com>
2019-06-16 13:22:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-function dwarfRollCall(array){
-  let dwarfes = "";
-  for(let i = 0; i < array.length; i++){
-    dwarfes += (i + 1 + ". " +  array[i] + " ");
+function dwarfRollCall(arrayDwarves){
+  let stringDwarves = "";
+  for( let i = 0; i < arrayDwarves.length; i++){
+  arrayDwarves += ( i + 1 + ". " + arrayDwarves[i] + " " );
   }
-  return dwarfes;
-//'1. Dopey 2. Grumpy 3. Bashful '
+  return stringDwarves;
 }
+//"1. Doc 2. Dopey 3. Bashful 4. Grumpy "
 
-function summonCaptainPlanet(array){
-  let newArray = [];
-  for(let i = 0; i < array.length; i++){
-    newArray.push(array[i].toUpperCase() + "!");
-  }
-  return newArray;
-}
 
-function longPlaneteerCalls(arrayOfCalls){
-  let longPlaneteerCalls = false;
-  for(let i = 0; i < arrayOfCalls.length; i++){
-    if(arrayOfCalls[i].length > 4){
-      longPlaneteerCalls = true;
-      }
-   }
-  return longPlaneteerCalls;
-}
-
-function findTheCheese(arrayOfStrings){
-  let cheeses = ['gouda', 'cheddar', 'camembert'];
-  for(let i = 0; i < cheeses.length; i++){
-  for(let j = 0; j < arrayOfStrings.length; j++){
-    if(arrayOfStrings[j] === cheeses[i]){
-      return arrayOfStrings[j];
-    }
-   }
-  }
-  return 'no cheese!';
-}

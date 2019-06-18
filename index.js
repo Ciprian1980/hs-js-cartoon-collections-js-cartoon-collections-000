77c6eb583e14965c1f592b9613a8afdf7fba9849
Ciprian Popa <ciprian_popa2002@yahoo.com>
2019-06-18 09:16:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 function dwarfRollCall(arrayDwarves){
   let stringDwarves = "";
-  for( let i = 0; i < arrayDwarves.length; i++){
+  for( let i = 0; i < arrayDwarves.length/2; i++){
   stringDwarves += ( i + 1 + ". " + arrayDwarves[i] + " " );
   }
   return stringDwarves;
 }
 //"1. Doc 2. Dopey 3. Bashful 4. Grumpy "
 
-function summonCaptainPLanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls){
   let newArray = [];
   for(let i = 0; i < planeteerCalls.length; i++){
-    newArray.push(planeteerCalls[i].toUpperCase + "! " );
+    newArray.push(planeteerCalls[i].toUpperCase() + "!" );
   }
   return newArray;
 }
 
+function longPlaneteerCalls(arrayOfCalls){
+  let longPlaneteerCalls = false;
+  for(let i = 0; i < arrayOfCalls.length; i++){
+    if(arrayOfCalls[i].length > 4)
+      longPlaneteerCalls = true;
+  }
+  return longPlaneteerCalls
+}
 
+function findTheCheese(arrayOfStrings){
+let cheeses = ['gouda', 'cheddar', 'camembert']
+for(let i = 0; i < arrayOfStrings.length; i++){
+  for(let j = 0; j < cheeses.length; j++){
+    if(cheeses[j] === arrayOfStrings[i]){
+      return arrayOfStrings[i];
+      }
+    }
+  }
+  return 'no cheese!';
+}
+
+function wordsWithB(words){
+  let newWords = [];
+  for( let i = 0; i < words.length; i++){
+    if(words[i][0] === 'b'){
+      newWords.push(words[i]);
+    }
+  }
+  return newWords;
+}
